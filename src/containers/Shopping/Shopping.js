d938b685f088f44d05932ce976d152a877a8cae6
Arash Yasaman <ym.arash@gmail.com>
2021-07-28 15:27:32 +0430

    add firebase database and use axios

--- src/containers/Shopping/Shopping.js
+++ src/containers/Shopping/Shopping.js
@@ -3,6 +3,8 @@ import Wrapper from '../../hoc/Wrapper'
 import Controls from '../../components/Controls/Controls'
 import Modal from '../../UI/Modal/Modal'
 import Order from '../../components/Order/Order'
+import axios from '../../axios-order'
+import Loader from '../../components/Loader/Loader'
 
 const prices = {
     کتاب: 11, 
@@ -13,15 +15,19 @@ const prices = {
 
 class Shopping extends React.Component {
     state = {
-        products: {
-           کتاب: 0, 
-           دفتر: 0,
-           مداد: 0,
-           خودکار: 0,
-        },
+        products: null,
         totalPrice: 0,
-        purchased: false
+        purchased: false,
+        loader: false
     }    
+
+    componentDidMount() {
+        axios
+            .get('https://shop-react-2c9bc-default-rtdb.firebaseio.com/products.json')
+            .then((response) => {
+                this.setState({ products: response.data })
+            })
+    }
 
     addProductHandler = (type) => {
         const prevCount = this.state.products[type]
@@ -59,31 +65,63 @@ class Shopping extends React.Component {
     modalCloseHandler = () => {
         this.setState({ purchased: false })
     }
-    purchaseContinueHandler = () => {
-        alert('خرید شما با موفقیت ثبت شد')
-        this.setState({ purchased: false })
+    purchaseContinueHandler = () => {      
+        this.setState({ loader: true })
+        const order = {
+            products: this.state.products,
+            totalPrice: this.state.totalPrice,
+            customer: {
+                name: 'Arash Yasaman',
+                email: 'ym.arash@gmail.com',
+            }
+        }
+        axios.post('/orders.json', order)
+        .then((response) => {
+            this.setState({ loader: false, purchased: false })
+            alert('خرید شما با موفقیت ثبت شد')
+            console.log(response)
+        })
+        .catch((response) => {
+            this.setState({ loader: false, purchased: false })
+            alert('متاسفانه خرید شما ثبت نشد')
+            console.log(response)
+        })
     }
  
     render() {
+        let order = null
+
+        if(this.state.loader) {
+            order = <Loader />
+        }
+
+        let controls = <Loader/>
+
+        if(this.state.products) {
+            controls = <Controls
+                    productAdd={this.addProductHandler}
+                    productRemove={this.removeProductHandler}
+                    price={this.state.totalPrice}
+                    order={this.purchasedHandler}
+                />
+
+            order = <Order
+                products={this.state.products}
+                price={this.state.totalPrice}
+                continue={this.purchaseContinueHandler}
+                cancel={this.modalCloseHandler}
+            />    
+        }
+
         return (
             <Wrapper>
                 <Modal
                     show={this.state.purchased}
                     modalClose={this.modalCloseHandler}
                 >
-                    <Order
-                        products={this.state.products}
-                        price={this.state.totalPrice}
-                        continue={this.purchaseContinueHandler}
-                        cancel={this.modalCloseHandler}
-                    />
+                {order}
                 </Modal>
-                <Controls
-                    productAdd={this.addProductHandler}
-                    productRemove={this.removeProductHandler}
-                    price={this.state.totalPrice}
-                    order={this.purchasedHandler}
-                />
+                {controls}
             </Wrapper>
         )
     }
